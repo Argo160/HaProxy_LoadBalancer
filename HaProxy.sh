ad757c37f7f70a805e66681eca27191c6f969f99
Argo160 <116899376+Argo160@users.noreply.github.com>
2024-01-27 23:08:00 +0330

    Update HaProxy.sh

--- HaProxy.sh
+++ HaProxy.sh
@@ -95,7 +95,7 @@ is_ipv4() {
 add_ip() {
     # Check if at least one port is specified in both backend and frontend sections
 #    if ! grep -qE "^\s*server\s+\w+\s+\d+\.\d+\.\d+\.\d+:[0-9]+\s*$" /etc/haproxy/haproxy.cfg; then
-if ! grep -qE '^ *bind \*:[0-9]+' /etc/haproxy/haproxy.cfg;
+if ! grep -qE '^ *bind \*:[0-9]+' /etc/haproxy/haproxy.cfg; then
         echo "Please specify at least one port in the HAProxy configuration file before adding IP addresses."
         return
     fi
